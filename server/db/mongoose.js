90c8b706102e093e8dbcf8758e6dd6f2db815693
mohannadRaslan <raslanmohannad@gmail.com>
2019-01-24 22:54:37 +0400

    Add useMongoClient to connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 //tell mongoose to user promise instade of callback
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoapp', {
+  useMongoClient: true,
+});
 
 
 module.exports = {mongoose};
